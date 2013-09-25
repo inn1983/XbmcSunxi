e7d9567961e1982b39115659ba8dcebc73713f60
rellla <ichgeh@imkreisrum.de>
2013-09-25 12:02:32 -0400

    [A10] Revert commit 26ca541c80
    Seems that CEDARV_COMMAND_RESET is wrong here. Rechange to FLUSH instead.
    Android behaviour remains unchanged and calls RESET, because FLUSH isn't defined in
    Android's libcedarv.h

--- xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecA10.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecA10.cpp
@@ -538,7 +538,11 @@ void CDVDVideoCodecA10::Reset()
   cedarv_picture_t pict;
 
   CLog::Log(LOGDEBUG, "A10: reset requested");
+#if defined(TARGET_ANDROID)
   m_hcedarv->ioctrl(m_hcedarv, CEDARV_COMMAND_RESET, 0);
+#else
+  m_hcedarv->ioctrl(m_hcedarv, CEDARV_COMMAND_FLUSH, 0);
+#endif
   while(m_hcedarv->display_request(m_hcedarv, &pict) == 0)
     m_hcedarv->display_release(m_hcedarv, pict.id);
   A10VLFreeQueue();
